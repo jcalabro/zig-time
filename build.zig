f19d9d8961898ce93243a92d32a266b84a3bb76a
Jim Calabro <jamesrcalabro@gmail.com>
2024-01-13 21:55:44 -0500

    update to latest zig

--- build.zig
+++ build.zig
@@ -1,23 +1,26 @@
 const std = @import("std");
 
-pub fn build(b: *std.build.Builder) void {
-    const time = b.addModule("time", .{
-        .source_file = .{ .path = "time.zig" },
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const time_mod = b.addModule("time", .{
+        .root_source_file = .{ .path = "time.zig" },
+        .target = target,
+        .optimize = optimize,
     });
 
     {
-        const target = b.standardTargetOptions(.{});
-        const optimize = b.standardOptimizeOption(.{});
-
         const tests = b.addTest(.{
             .root_source_file = .{ .path = "main.zig" },
             .target = target,
             .optimize = optimize,
         });
-        tests.addModule("time", time);
 
         const run_tests = b.addRunArtifact(tests);
         const tests_step = b.step("test", "Run all the tests.");
         tests_step.dependOn(&run_tests.step);
+
+        tests.root_module.addImport("time", time_mod);
     }
 }
